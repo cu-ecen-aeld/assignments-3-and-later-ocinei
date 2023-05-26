5a46ed9675a95f5ac152442ea6ead8e17d68e8f5
HephaesVMforLinux <ubuntu@ip-172-31-43-17.ap-southeast-2.compute.internal>
2023-05-26 15:27:30 +0000

    message

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,9 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *username_var;
+    const char *comparend;
+    username_var = my_username();
+    comparend = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(username_var, comparend, "Message");
 }
